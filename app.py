df1fe6ac5fb010a0c09a7255553475d643d33698
brudergabriel <gabriel.bruder@goomer.com.br>
2026-03-06 10:21:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -231,21 +231,21 @@ elif not st.session_state.test_finished:
 
     user_input = st.chat_input("Digite sua resposta")
 
-    if user_input:
+if user_input:
 
-        st.session_state.messages.append(
-            {"role": "analista", "content": user_input}
-        )
+    st.session_state.messages.append(
+        {"role": "analista", "content": user_input}
+    )
 
-        st.session_state.interaction_count += 1
+    st.session_state.interaction_count += 1
 
-        client_reply = get_client_llm_response(user_input)
+    client_reply = get_client_llm_response(user_input)
 
-        st.session_state.messages.append(
-            {"role": "cliente", "content": client_reply}
-        )
+    st.session_state.messages.append(
+        {"role": "cliente", "content": client_reply}
+    )
 
-        st.rerun()
+    st.rerun()
 
     # FINALIZAR TESTE
 
